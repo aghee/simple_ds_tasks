cd305ad4936bd74cb766790674611f25b0b03970
aghee <agymunzala@gmail.com>
2023-08-20 02:00:29 +0300

    File showing histogram from dictionary

---
+++ dict_hist_one.py
@@ -0,0 +1,11 @@
+counts=dict()
+names=['a','b','a','d','l','m','a','l','n ','n ','n ','d','d','a','z','a','d','m','b','a','a','a']
+for name in names:
+    counts[name]=counts.get(name,0)+1
+print(counts)
+#import matplotlib, note use jupyterlab for histogram
+import matplotlib.pyplot as plt
+keys=list(counts.keys())
+values=list(counts.values())
+plt.bar(range(len(counts)),values,tick_label=keys)
+plt.show()
