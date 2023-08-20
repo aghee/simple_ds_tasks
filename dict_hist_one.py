794f0908387ea8f7acd6ad5cd7a70208a2545fcf
aghee <agymunzala@gmail.com>
2023-08-20 09:16:50 +0300

    opening file and creating dictionary

--- dict_hist_one.py
+++ dict_hist_one.py
@@ -1,3 +1,5 @@
+"""
+#fewer values in dictionary
 counts=dict()
 names=['a','b','a','d','l','m','a','l','n ','n ','n ','d','d','a','z','a','d','m','b','a','a','a']
 for name in names:
@@ -8,4 +10,22 @@ import matplotlib.pyplot as plt
 keys=list(counts.keys())
 values=list(counts.values())
 plt.bar(range(len(counts)),values,tick_label=keys)
+plt.show()
+"""
+#more values in dictionary
+fname=input('enter file name:')
+fhand=open(fname)
+#print(fhand.read())
+
+for name in fhand:
+    var1=name.split()
+    #print(var1)
+dictone={}
+for name in var1:
+    dictone[name]=dictone.get(name,0)+1
+print(dictone)
+import matplotlib.pyplot as plt
+keys=list(dictone.keys())
+values=list(dictone.values())
+plt.bar(range(len(dictone)),values,tick_label=keys)
 plt.show()
