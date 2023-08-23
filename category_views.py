0d04725d8ccd1a63d237a67f1666c7d743d870c3
aghee <agymunzala@gmail.com>
2023-08-24 02:34:48 +0300

    Renamed file csvanalysis.py to category_views.py

---
+++ category_views.py
@@ -0,0 +1,45 @@
+from openpyxl import load_workbook
+from matplotlib import pyplot as plt
+#column_one A
+wb = load_workbook("Book1.xlsx") #workbookname
+ws = wb['Sheet6'] #worksheetname
+column_one = ws['A'] #ColumnA
+column_one_list = [column_one[x].value for x in range(len(column_one))] #column_one A as a list
+print('The first list for keys is:',column_one_list)
+
+#column_two B
+column_two=ws['B']#columnB
+column_two_list=[column_two[x].value for x in range(len(column_two))]#column_two B as a list
+print('The second list for values is:',column_two_list)
+print('There are',end=' ')
+print(len(column_two_list),end=' ') 
+print('category items in this workbook')
+
+#piechart
+fig = plt.figure(figsize =(10, 7))
+plt.pie(column_two_list, labels=column_one_list)
+#plt.show()
+
+
+#Create a dictionary from the two lists
+#using nested for loop - naive method
+dictone={}
+for k in column_one_list:
+    for v in column_two_list:
+        dictone[k]=v
+        column_two_list.remove(v)
+        #if len(column_two_list):
+            #break            
+        #if bool(column_two_list):
+            #break
+        break
+
+print('The dictionary is', dictone)
+#retrieve a list of all keys
+keys=list(dictone.keys())
+#retrieve a list of all values
+values=list(dictone.values())
+#bar graph
+#plt.bar(range(len(dictone)),values,tick_label=keys)
+#display bar graph
+plt.show()
